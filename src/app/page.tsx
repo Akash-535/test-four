fcf9e66cbf5d638f4725284f31f1ffedf29a85bd
akash-535 <akashbish9e@gmail.com>
2025-03-12 09:17:19 +0530

    suspense removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 import Login from "@/components/login/Login";
-import { Suspense } from "react";
 
 export default function Home() {
-  return (
-    <Suspense>
-      <Login />
-    </Suspense>
-  );
+  return <Login />;
 }
